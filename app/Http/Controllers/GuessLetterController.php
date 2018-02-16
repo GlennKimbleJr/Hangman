fff85fa3e87b6a2d01db4b29a5f5e02d1e148747
GlennKimbleJr <glennkimble@yahoo.com>
2018-02-16 00:38:47 -0500

    Refactored Guess Controllers

--- app/Http/Controllers/GuessLetterController.php
+++ app/Http/Controllers/GuessLetterController.php
@@ -20,28 +20,57 @@ class GuessLetterController extends Controller
 
         $game = $user->getActiveGame();
         $isCorrect = $game->guessLetter($request->guess);
-
         $game->getActiveRound()->guesses()->create([
             'guess' => $request->guess,
             'is_correct' => $isCorrect,
         ]);
 
-        if (!$isCorrect && $game->getActiveRound()->maxGuessesReached()) {
-            Session::flash('error', 'failed');
-            $game->getActiveRound()->markAsLost();
-
-            return redirect()->to(route('play'));
+        if (!$this->gameIsComplete($game, $isCorrect)) {
+            Session::flash(
+                $isCorrect ? 'success' : 'error',
+                $isCorrect ? 'success' : 'miss'
+            );
         }
-
-        if ($isCorrect && $game->getActiveRound()->allLettersGuessed()) {
-            $game->getActiveRound()->markAsWon();
-        }
-
-        Session::flash(
-            $isCorrect ? 'success' : 'error',
-            $isCorrect ? 'success' : 'miss'
-        );
 
         return redirect()->to(route('play'));
+    }
+
+    private function gameIsComplete($game, $isCorrect)
+    {
+        if ($this->gameIsLost($game, $isCorrect)) {
+            return true;
+        }
+
+        if ($this->gameIsWon($game, $isCorrect)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function gameIsLost($game, $isCorrect)
+    {
+        if (!$isCorrect && $game->getActiveRound()->maxGuessesReached()) {
+            Session::flash('error', 'failed');
+
+            $game->getActiveRound()->markAsLost();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function gameIsWon($game, $isCorrect)
+    {
+        if ($isCorrect && $game->getActiveRound()->allLettersGuessed()) {
+            Session::flash('success', 'won');
+
+            $game->getActiveRound()->markAsWon();
+
+            return true;
+        }
+
+        return false;
     }
 }
--- app/Http/Controllers/GuessPhraseController.php
+++ app/Http/Controllers/GuessPhraseController.php
@@ -20,28 +20,57 @@ class GuessPhraseController extends Controller
 
         $game = $user->getActiveGame();
         $isCorrect = $game->guessPhrase($request->guess);
-
         $game->getActiveRound()->guesses()->create([
             'guess' => $request->guess,
             'is_correct' => $isCorrect,
         ]);
 
-        if (!$isCorrect && $game->getActiveRound()->maxGuessesReached()) {
-            Session::flash('error', 'failed');
-            $game->getActiveRound()->markAsLost();
-
-            return redirect()->to(route('play'));
+        if (!$this->gameIsComplete($game, $isCorrect)) {
+            Session::flash(
+                $isCorrect ? 'success' : 'error',
+                $isCorrect ? 'success' : 'miss'
+            );
         }
-
-        if ($isCorrect) {
-            $game->getActiveRound()->markAsWon();
-        }
-
-        Session::flash(
-            $isCorrect ? 'success' : 'error',
-            $isCorrect ? 'success' : 'miss'
-        );
 
         return redirect()->to(route('play'));
+    }
+
+    private function gameIsComplete($game, $isCorrect)
+    {
+        if ($this->gameIsLost($game, $isCorrect)) {
+            return true;
+        }
+
+        if ($this->gameIsWon($game, $isCorrect)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function gameIsLost($game, $isCorrect)
+    {
+        if (!$isCorrect && $game->getActiveRound()->maxGuessesReached()) {
+            Session::flash('error', 'failed');
+
+            $game->getActiveRound()->markAsLost();
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public function gameIsWon($game, $isCorrect)
+    {
+        if ($isCorrect) {
+            Session::flash('success', 'won');
+
+            $game->getActiveRound()->markAsWon();
+
+            return true;
+        }
+
+        return false;
     }
 }
